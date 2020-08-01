e92228e28ca3e124e1589b292fbe8e0aa5c6ec4f
jasuriy <jasur1998gopirjonov@gmail.com>
2020-08-01 10:40:24 +0500

    Updates

---
+++ precache-manifest.81e20bfeeafe5189a2b25b8b3985d628.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d42c95a0f795aabe8a4d1b3baca5b99",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "7b9da5623c1da69e8bb8",
+    "url": "/movie-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cb6f6a58ffdb804e3f11",
+    "url": "/movie-app/static/css/main.e46db61a.chunk.css"
+  },
+  {
+    "revision": "7b9da5623c1da69e8bb8",
+    "url": "/movie-app/static/js/2.a573b93b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.a573b93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6f6a58ffdb804e3f11",
+    "url": "/movie-app/static/js/main.f3eb514d.chunk.js"
+  },
+  {
+    "revision": "73d5f12738c839e7e9c3",
+    "url": "/movie-app/static/js/runtime-main.d4a17023.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/movie-app/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/movie-app/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/movie-app/static/media/spinner.235b9b3e.gif"
+  }
+]);
